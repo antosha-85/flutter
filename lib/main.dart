4e6ce9186601203c3c089c86a26017ec7ba9add7
Anton Smirnov <antosha-85@yandex.ru>
2021-05-01 14:05:24 -0600

    Added body, with a few columns, implemented mainAxisAlignment and crossAxixAlignment

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,31 @@ void main() {
       appBar: AppBar(
         title: Text("Hello again! "),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            child: Text("Hello World1!"),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Text("Hello World2!"),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text("Hello World3! Hello Friends!"),
+          ),
+        ]
+
+      )
     ),
   ));
 }
