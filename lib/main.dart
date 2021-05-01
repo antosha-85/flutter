fe367c983ea73147cd813c61a15d56867a145928
Anton Smirnov <antosha-85@yandex.ru>
2021-05-01 12:41:14 -0600

    Initialized a new flutter project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello again! "),
+      ),
+    ),
+  ));
+}
+
